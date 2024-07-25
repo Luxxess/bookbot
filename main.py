cb08cb3e07439c800132e743c32d1292c7ef8b18
Luxxess <enricozenker@gmail.com>
2024-07-25 13:06:27 +0200

    Finished Project

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_words = get_count_words(text)
-    #print(f"{count_words} words found in the document")
-    charachters = get_count_characters(text)
-    #print(f"The book has {charachters} of each character")
-    sort_dict(charachters)
-
+    chars_dict = get_count_characters(text)
+    sort_dict(chars_dict)
+    list_dict = sort_list(chars_dict)
+    output(count_words, list_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -33,8 +32,30 @@ def get_count_characters(text):
 
     return characters
     
-def sort_dict(characters):
-    for c in characters:
-        print(characters[c])
-        
+def sort_dict(chars_dict):
+
+    new_dict = {}
+    for c in chars_dict:
+        new_dict[c] = new_dict["num"] = chars_dict[c]
+    return new_dict["num"]
+
+def sort_list(chars_dict):
+    list_dict = []
+    for c in chars_dict:
+        temp = {}
+        temp[c] = chars_dict[c]
+        list_dict.append(temp)
+    list_dict.sort(reverse=True, key=sort_dict)
+    return (list_dict)
+
+def output(count_words, list_dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words} words found in the document\n")
+    for letter in list_dict:
+        for char in letter:
+            print(f"The '{char}' character was found {letter[char]} times")
+    print("--- End report ---")
+    return 0
+
+
 main()
